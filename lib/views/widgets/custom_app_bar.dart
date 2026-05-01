adb6d936171a440d85224476a388e8dd8da5b1a6
Mohamad <hamwimohamad200@gmail.com>
2026-05-01 19:05:06 +0300

    create a appbar_Search icon add a Poppins font and build a card model

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22),
+      child: Column(
+        children: [
+          SizedBox(height:50),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: Colors.amberAccent,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text('Flutter tips',style: TextStyle(color: Colors.black,fontSize: 26),),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16 , bottom: 10),
+              child: Text('Build your career with Tharwat sami' , style: TextStyle(color:  Colors.black.withOpacity(.4) , fontSize: 20),),
+            ),
+            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete) , color: Colors.black, iconSize: 30,),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 24),
+            child: Text(
+                'May 20, 2023',
+              style: TextStyle(
+                fontSize: 16,
+                  color: Colors.black.withOpacity(.4)
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
