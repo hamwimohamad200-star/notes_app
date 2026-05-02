d5e52ffb6a59ef03c1723ac8c18a7e9a0807306b
Mohamad <hamwimohamad200@gmail.com>
2026-05-02 09:49:02 +0300

    create notes List View

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -13,48 +15,14 @@ class NotesViewBody extends StatelessWidget {
         children: [
           SizedBox(height:50),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: Colors.amberAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text('Flutter tips',style: TextStyle(color: Colors.black,fontSize: 26),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16 , bottom: 10),
-              child: Text('Build your career with Tharwat sami' , style: TextStyle(color:  Colors.black.withOpacity(.4) , fontSize: 20),),
-            ),
-            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete) , color: Colors.black, iconSize: 30,),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text(
-                'May 20, 2023',
-              style: TextStyle(
-                fontSize: 16,
-                  color: Colors.black.withOpacity(.4)
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+
 
