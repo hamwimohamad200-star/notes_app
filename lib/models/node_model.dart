4defab08da2a92607f71ad979a4601ae34175479
Mohamad <hamwimohamad200@gmail.com>
2026-05-07 20:40:41 +0300

    Create node model

---
+++ lib/models/node_model.dart
@@ -0,0 +1,13 @@
+class NodeModel {
+  final String title ;
+  final String subtitle ;
+  final String date ;
+  final int color ;
+
+  NodeModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color
+  });
+}
